66773a25da960ae80c1d0556efd7a7a458213e2a
knightwo <ka7110zuki@gmail.com>
2023-02-03 14:20:50 +0900

    ログイン画面にフォワードするメソッドを作成

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -3,13 +3,13 @@ package com.example.controller;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
+import com.example.form.LoginForm;
 import com.example.service.AdministratorService;
 
 /**
@@ -27,8 +27,7 @@ public class AdministratorController {
 	/**
 	 * administrator/insert.htmlにフォワードする.
 	 * 
-	 * @param form  管理者登録するための情報
-	 * @param model リクエストスコープ
+	 * @param form  リクエストスコープ
 	 * @return フォワード先
 	 */
 	@GetMapping("/toInsert")
@@ -49,6 +48,17 @@ public class AdministratorController {
 		BeanUtils.copyProperties(form, administrator);
 		administratorService.insert(administrator);
 		return "redirect:/";
+	}
+	
+	/**
+	 *  administrator/loginにフォワードする.
+	 * 
+	 * @param form リクエストスコープ
+	 * @return フォワード先
+	 */
+	@GetMapping("/")
+	public String toLogin(LoginForm form) {
+		return "administrator/login";
 	}
 
 }
