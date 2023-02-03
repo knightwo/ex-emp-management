c3b055b55750032362e5dfd7407c493087365f63
knightwo <ka7110zuki@gmail.com>
2023-02-03 14:07:42 +0900

    管理者情報を登録するメソッドを作成

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,18 +1,21 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 
 /**
  * 管理者情報を操作するコントローラ.
  * 
- * @author ka711 
+ * @author ka711
  *
  */
 @Controller
@@ -32,6 +35,20 @@ public class AdministratorController {
 	public String toInsert(InsertAdministratorForm form) {
 		return "administrator/insert";
 
+	}
+
+	/**
+	 * 管理者情報を登録する.
+	 * 
+	 * @param form 管理者登録するための情報
+	 * @return リダイレクト先
+	 */
+	@PostMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator administrator = new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+		administratorService.insert(administrator);
+		return "redirect:/toInsert";
 	}
 
 }
