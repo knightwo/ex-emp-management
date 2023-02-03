37ddc4e2199c627793d56c9bc4b7e597a413fe80
knightwo <ka7110zuki@gmail.com>
2023-02-03 12:27:11 +0900

    管理者情報登録時に使用数るフォームを作成

--- src/main/java/com/example/repository/EmployeeRepository.java
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -70,11 +70,19 @@ public class EmployeeRepository {
 	 * @param employee 従業員情報
 	 */
 	public void update(Employee employee) {
-		String sql = "UPDATE employees SET " + "name             = :name," + "image            = :image,"
-				+ "gender           = :gender" + ",hire_date       = :hireDate," + "mail_address     = :mailAddress,"
-				+ "zip_code         = :zipCode," + "address          = :address," + "telephone        = :telephone,"
-				+ "salary           = :salary," + "characteristics  = :characteristics,"
-				+ "dependents_count = :dependentsCount" + "WHERE id         = :id;";
+		String sql = "UPDATE employees SET " 
+				+ "name             = :name," 
+				+ "image            = :image,"
+				+ "gender           = :gender" 
+				+ ",hire_date       = :hireDate," 
+				+ "mail_address     = :mailAddress,"
+				+ "zip_code         = :zipCode," 
+				+ "address          = :address," 
+				+ "telephone        = :telephone,"
+				+ "salary           = :salary," 
+				+ "characteristics  = :characteristics,"
+				+ "dependents_count = :dependentsCount" 
+				+ "WHERE id         = :id;";
 		SqlParameterSource param = new BeanPropertySqlParameterSource(employee);
 		template.update(sql, param);
 
